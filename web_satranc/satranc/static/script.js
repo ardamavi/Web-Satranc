2ce7109f09554dabf0ba5c7e980caebb01163d4d
Arda Mavi <ardamavi2@gmail.com>
2016-08-09 15:29:51 +0300

    Kullanıcı Arayüzü Güncellendi

--- web_satranc/satranc/static/script.js
+++ web_satranc/satranc/static/script.js
@@ -14,3 +14,37 @@ $( function() {
     $(".black").droppable();
 
   } );
+
+
+
+var dragstartHandler = targetElement.ondragstart;
+
+var man = document.getElementsByClassName(dragstartHandler);
+var startId = man[man.length - 1].parentNode.id;
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    {% csrf_token %}
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
+post('', {tasKonum: startId, oynanacakYer: stopId});
--- web_satranc/satranc/views.py
+++ web_satranc/satranc/views.py
@@ -14,9 +14,16 @@ socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # SOCK_DGRAM -> Protokol tipi UDP
 
 
-
 # Create your views here.
 class SatrancView(View):
+    def create_div(self, pair):
+        if pair[0] == 'B':
+            return '<div class="blackMan">' + pair[1] + '</div>'
+        elif pair[0] == 'W':
+            return '<div class="whiteMan">' + pair[1] + '</div>'
+        else:
+            return ''
+
     def get(self, request):
         socket_server.sendto(b'Arda Mavi', (haberlesme_ip, haberlesme_port))
         # Tahta Bilgisi :
@@ -29,53 +36,55 @@ class SatrancView(View):
         for index in range(5):
             del tahta[10]
 
-        for string in tahta:
-            eski_str = string
-            yeni_s = ""
+        array = []
 
-            renk_geldi = False
-            renk_str = ""
-            span_counter = 0
-            char_counter = 0
-            char_geldi = False
-            for s in string:
-                if renk_geldi and renk_str in ['[97m', '[44m', '[46m', '[106m', '[30m']:
-                    renk_geldi = False
-                    if renk_str == '[97m':
-                        yeni_s += '' # <span style="color:#ececec">
-                    elif renk_str == '[30m':
-                        yeni_s += '' # <span style="color:#081115">
-                    span_counter += 1
-                    char_geldi = True
-                    renk_str = ""
-                    if s == "\x1b":
-                        renk_geldi = True
-                        char_geldi = False
+        for input in tahta[1:-1]:
+            i = 0
+
+            arr = []
+
+            while i < 8:
+                arr.append(['',''])
+                i += 1
+
+            i = 0
+
+            for index in range(13,len(input)-3):
+                if input[index] == " " and input[index+2] == " ":
+                    if input[index-2] == "0":
+                        arr[i][0] = "B"
+                    elif input[index-2] == "7":
+                        arr[i][0] = "W"
                     else:
-                        yeni_s += s
-                    continue
-                elif renk_geldi:
-                    renk_str += s
-                    continue
-                if char_geldi and char_counter < 2:
-                    yeni_s += s
-                    char_counter += 1
-                    if char_counter == 2:
-                        while span_counter != 0:
-                            yeni_s += '' # </span>
-                            span_counter -= 1
-                        char_counter = 0
-                        char_geldi = False
-                    continue
-                if s == "\x1b":
-                    renk_geldi = True
-                else:
-                    yeni_s += s
+                        arr[i][0] = "E"
 
-            string = yeni_s
-            tahta = [w.replace(eski_str, string) for w in tahta]
+                    arr[i][1] = input[index+1]
+                    i+=1
 
-        tahta[0] = tahta[9] = """<span style="color:#ececec"><span style="background-color:#1c28bd">    A  B  C  D  E  F  G  H    </span></span>"""
+            array.append(arr)
+
+        tahta = []
+        letter = 'a'
+        color = "black"
+        number = 1
+
+        while number != 9:
+            if letter == 'a':
+                div = '<div class="location">' + str(number) + '</div>'
+                tahta.append(div)
+            position = str(number) + letter
+            div = '<div class="' + color + '" id="' + position + '">' + self.create_div(array[number - 1][ord(letter) - ord('a')]) + '</div>'
+            tahta.append(div)
+            if color == "black": color = "white"
+            elif color == "white": color = "black"
+            letter = chr(ord(letter) + 1)
+            if letter == 'i':
+                div = '<div class="location">' + str(number) + '</div>'
+                tahta.append(div)
+                if color == "black": color = "white"
+                elif color == "white": color = "black"
+                number += 1
+                letter = 'a'
 
         return render(request, 'satranc.html', {'tahta': tahta})
 
@@ -110,52 +119,54 @@ class SatrancView(View):
             for index in range(7):
                 del tahta[10]
 
-        for string in tahta:
-            eski_str = string
-            yeni_s = ""
+        array = []
 
-            renk_geldi = False
-            renk_str = ""
-            span_counter = 0
-            char_counter = 0
-            char_geldi = False
-            for s in string:
-                if renk_geldi and renk_str in ['[97m', '[44m', '[46m', '[106m', '[30m']:
-                    renk_geldi = False
-                    if renk_str == '[97m':
-                        yeni_s += '' # <span style="color:#ececec">
-                    elif renk_str == '[30m':
-                        yeni_s += '' # <span style="color:#081115">
-                    span_counter += 1
-                    char_geldi = True
-                    renk_str = ""
-                    if s == "\x1b":
-                        renk_geldi = True
-                        char_geldi = False
+        for input in tahta[1:-1]:
+            i = 0
+
+            arr = []
+
+            while i < 8:
+                arr.append(['',''])
+                i += 1
+
+            i = 0
+
+            for index in range(13,len(input)-3):
+                if input[index] == " " and input[index+2] == " ":
+                    if input[index-2] == "0":
+                        arr[i][0] = "B"
+                    elif input[index-2] == "7":
+                        arr[i][0] = "W"
                     else:
-                        yeni_s += s
-                    continue
-                elif renk_geldi:
-                    renk_str += s
-                    continue
-                if char_geldi and char_counter < 2:
-                    yeni_s += s
-                    char_counter += 1
-                    if char_counter == 2:
-                        while span_counter != 0:
-                            yeni_s += '' # </span>
-                            span_counter -= 1
-                        char_counter = 0
-                        char_geldi = False
-                    continue
-                if s == "\x1b":
-                    renk_geldi = True
-                else:
-                    yeni_s += s
+                        arr[i][0] = "E"
 
-            string = yeni_s
-            tahta = [w.replace(eski_str, string) for w in tahta]
+                    arr[i][1] = input[index+1]
+                    i+=1
 
-        tahta[0] = tahta[9] = """<span style="color:#ececec"><span style="background-color:#1c28bd">    A  B  C  D  E  F  G  H    </span></span>"""
+            array.append(arr)
 
-        return render(request, 'satranc.html', {'tahta': tahta} )
+        tahta = []
+        letter = 'a'
+        color = "black"
+        number = 1
+
+        while number != 9:
+            if letter == 'a':
+                div = '<div class="location">' + str(number) + '</div>'
+                tahta.append(div)
+            position = str(number) + letter
+            div = '<div class="' + color + '" id="' + position + '">' + self.create_div(array[number - 1][ord(letter) - ord('a')]) + '</div>'
+            tahta.append(div)
+            if color == "black": color = "white"
+            elif color == "white": color = "black"
+            letter = chr(ord(letter) + 1)
+            if letter == 'i':
+                div = '<div class="location">' + str(number) + '</div>'
+                tahta.append(div)
+                if color == "black": color = "white"
+                elif color == "white": color = "black"
+                number += 1
+                letter = 'a'
+
+        return render(request, 'satranc.html', {'tahta': tahta})
